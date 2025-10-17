ae0d97c274e564be66ba53061e88e46abad25b5c
Valerius Petrini <valeriuspetrini@gmail.com>
2025-10-17 17:06:36 +0000

    feat: get assignments

--- src/lib/components/Google.svelte
+++ src/lib/components/Google.svelte
@@ -8,7 +8,10 @@
         // @ts-ignore
         tokenClient = google.accounts.oauth2.initTokenClient({
             client_id: '315808916557-l71t37pku56pahaumffp45o11h1u145f.apps.googleusercontent.com',
-            scope: 'https://www.googleapis.com/auth/classroom.courses.readonly',
+            scope: [
+                'https://www.googleapis.com/auth/classroom.courses.readonly',
+                'https://www.googleapis.com/auth/classroom.coursework.students.readonly'
+            ].join(' '),
             callback: (token: any) => {
                 tokenResponse = token;
             },
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,8 +15,24 @@
         console.log(result);
         return result;
     }
+    
+    async function getCourseWork() {
+        if (!tokenResponse || !tokenResponse.access_token || !courseSelected)
+            return null;
+        const data = await fetch(`https://classroom.googleapis.com/v1/courses/${courseSelected}/courseWork`, {
+            headers: {
+                Authorization: `Bearer ${tokenResponse.access_token}`
+            }
+        });
+        const result = await data.json();
+        console.log(result);
+        return result;
+    }
 
     let classroomCourses: Promise<{courses: ClassroomCourse[]}> = $derived(getCourses());
+    let classroomCourseWork = $derived(getCourseWork());
+    
+    let courseSelected: number | null = $state(null);
 </script>
 <h1 class="text-center text-5xl p-4">Auto Java Grader</h1>
 
@@ -37,7 +53,7 @@
                 {#if courses && courses.courses && courses.courses.length > 0}
                     <label class="label">
                         <span class="label-text">Courses</span>
-                        <select class="select">
+                        <select class="select" bind:value={courseSelected}>
                             {#each courses.courses as course}
                                 <option value={course.id}>{course.name}</option>
                             {/each}
@@ -48,6 +64,24 @@
                 {/if}
             {:catch error}
                 <p class="text-red-500">Error fetching courses: {error.message}</p>
+            {/await}
+
+            {#await classroomCourseWork}
+                <p>Loading your classwork...</p>
+            {:then courseWork} 
+                <h2 class="text-2xl mt-4 mb-2">Next, select an assignment:</h2>
+                {#if courseWork && courseWork.courseWork && courseWork.courseWork.length > 0}
+                    <label class="label">
+                        <span class="label-text">Course Work</span>
+                        <select class="select" bind:value={courseSelected}>
+                            {#each courseWork.courseWork as work}
+                                <option value={work.id}>{work.title}</option>
+                            {/each}
+                        </select>
+                    </label>
+                {:else}
+                    <p>Couldn't find any coursework.</p>
+                {/if}
             {/await}
         {/if}
     </div>
