4a0e32e4342412b4dc63cd69700fc1698b2d3186
Villy-P <valeriuspetrini@gmail.com>
2025-10-20 00:16:47 -0500

    feat: wrap readonly textarea in a label for improved accessibility

--- src/lib/components/Dashboard.svelte
+++ src/lib/components/Dashboard.svelte
@@ -113,6 +113,8 @@
         {/await}
     </div>
     <div class="w-2/3">
-        <textarea class="bg-[#121212] text-area w-full h-full p-4 font-mono" readonly>{selectedStudent?.javaContent}</textarea>
+        <label class="label h-full">
+            <textarea class="bg-[#121212] text-area w-full h-full p-4 font-mono" readonly>{selectedStudent?.javaContent}</textarea>
+        </label>
     </div>
 </div>
