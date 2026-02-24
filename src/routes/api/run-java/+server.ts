87fc6f516875eb6e719b27c51d16f36f7bcefe39
Valerius Petrini <valeriuspetrini@gmail.com>
2026-02-24 14:47:16 +0000

    feat: add cleanup of output directory after compilation and execution errors

--- src/routes/api/run-java/+server.ts
+++ src/routes/api/run-java/+server.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import child_process from 'child_process';
-import {promises} from 'fs';
+import {promises, rmSync} from 'fs';
 import { fileURLToPath } from 'url';
 import { promisify } from 'util';
 
@@ -26,16 +26,18 @@ export async function POST({ request }) {
         await exec(`javac "${filePath}"`);
     } catch (err: any) {
         const stderr = err?.stderr || err?.message || String(err);
+        await promises.rm(`${OUT_DIR}/${path.parse(fileName).name}`, { recursive: true, force: true });
         return new Response(`Compilation error: ${stderr}`, { status: 500 });
     }
 
     try {
-        console.log("OUTPUT DONE");
         const className = path.parse(fileName).name;
         const { stdout } = await exec(`java -cp . ${className}`, { cwd: `${OUT_DIR}/${className}` });
+        await promises.rm(`${OUT_DIR}/${path.parse(fileName).name}`, { recursive: true, force: true });
         return new Response(`Program output:\n${stdout}`);
     } catch (err: any) {
         const stderr = err?.stderr || err?.message || String(err);
+        await promises.rm(`${OUT_DIR}/${path.parse(fileName).name}`, { recursive: true, force: true });
         return new Response(`Runtime error: ${stderr}`, { status: 500 });
     }
 }
