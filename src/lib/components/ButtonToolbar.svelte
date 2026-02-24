de79a0ef51b8a464521c1b129e156395d84bd489
Valerius Petrini <valeriuspetrini@gmail.com>
2026-02-24 21:05:09 +0000

    feat: add top navbar with button toolbar for improved user interaction

---
+++ src/lib/components/ButtonToolbar.svelte
@@ -0,0 +1,17 @@
+<script>
+    import LogOut from "@lucide/svelte/icons/log-out";
+    import RefreshCCW from "@lucide/svelte/icons/refresh-ccw";
+    import { Tooltip } from "flowbite-svelte";
+
+    function logout() {
+        document.cookie.split(";").forEach(function(c) { 
+            document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); 
+        });
+        location.reload();
+    }
+</script>
+
+<button type="button" class="btn-icon preset-filled w-fit" onclick={logout}><LogOut size={18}/></button>
+<Tooltip placement="bottom">Sign out</Tooltip>
+<button type="button" class="btn-icon preset-filled w-fit z-50 ml-auto"><RefreshCCW size={18}/></button>
+<Tooltip placement="bottom">Re-run Student Submission</Tooltip>
