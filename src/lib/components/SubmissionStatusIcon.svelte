450d6e24ae094b01a79cfc1db319c3e83b81f82b
Valerius Petrini <valeriuspetrini@gmail.com>
2026-02-24 21:08:27 +0000

    feat: add tooltips to submission status icons for improved user feedback

--- src/lib/components/SubmissionStatusIcon.svelte
+++ src/lib/components/SubmissionStatusIcon.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { SubmissionStatus } from "$lib/scripts/user";
+	import { Tooltip } from "flowbite-svelte";
 
     let { submissionStatus = $bindable() }: {
         submissionStatus: SubmissionStatus
@@ -9,11 +10,15 @@
 <div class="flex items-center justify-center ml-auto mr-3 h-5 w-5">
     {#if submissionStatus === SubmissionStatus.NOT_SUBMITTED}
         <span class="text-yellow-500">❔</span>
+        <Tooltip placement="right">Not submitted</Tooltip>
     {:else if submissionStatus === SubmissionStatus.RUNTIME_ERROR || submissionStatus === SubmissionStatus.COMPILE_ERROR}
         <span class="text-red-500">❌</span>
+        <Tooltip placement="right">Error in submission</Tooltip>
     {:else if submissionStatus === SubmissionStatus.WRONG_OUTPUT}
         <span class="text-orange-500">⚠️</span>
+        <Tooltip placement="right">Wrong output</Tooltip>
     {:else if submissionStatus === SubmissionStatus.SUCCESS}
         <span class="text-green-500">✅</span>
+        <Tooltip placement="right">Correct submission</Tooltip>
     {/if}
 </div>
